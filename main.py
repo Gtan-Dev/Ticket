81c678188e8a1d4f3095a8c5a2c755f18d8956c7
Gtan <105869793+Gtan-1@users.noreply.github.com>
2023-04-18 12:12:10 -0700

    insert and retrieve

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import mysql.connector
 
 app = Flask(__name__)
+
+
+# MySQL connection configuration
+config = {
+    'user': 'root',
+    'password': '',
+    'host': 'localhost',
+    'database': 'ticket_db'
+}
+
+# Create a MySQL connection object
+conn = mysql.connector.connect(**config)
+
+
+#Admin routes
 
 @app.route('/admin')
 def admin():
@@ -9,6 +25,24 @@ def admin():
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/regist', methods=['GET', 'POST'])
+def regist():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirm-password')
+
+    # Create a cursor object
+
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO `customer` (`username`, `email`, `phone`, `password`, `confirm_password`) VALUES(%s, %s, %s, %s, %s)"
+    value = (username, email, phone, password, confirm_password)
+    cursor.execute(sql, value)
+    conn.commit()
+    return render_template('home.html', success = 'User created successfully')
 
 @app.route('/events')
 def events():
@@ -42,9 +76,15 @@ def booking():
 def customers():
     return (render_template('admin/admin-customers.html'))
 
+#Route to retrieve events from database to admin page
 @app.route('/ad-events')
-def ad_events():
-    return (render_template('admin/ad-events.html'))
+def listEvents():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    cursor.close()
+    return (render_template('admin/ad-events.html', rows=rows))
+
 
 @app.route('/createevent')
 def createEvent():
@@ -65,17 +105,51 @@ def purchaseTicket():
 #route to add an event to database
 @app.route('/addEvent', methods=['GET', 'POST'])
 def store():
-    name = request.form['name']
-    date = request.form['date']
-    time = request.form['time']
-    location = request.form['location']
-    ticket_price = request.form['ticket_price']
-    # sql = "INSERT INTO `users` (`name`, `date`, `time`, `location`, `ticket_price`) VALUES(%s, %s, %s, %s, %s)"
-    # value = (name, date, time, location, ticket_price)
-    # mycursor.execute(sql, value)
-    # mydb.commit()
-    
-    return render_template('admin/listevent.html', error = 'Data inserted successfully')
+    name = request.form.get('name')
+    date = request.form.get('date')
+    time = request.form.get('time')
+    location = request.form.get('location')
+    ticket_price = request.form.get('ticket_price')
+
+     # Create a cursor object
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO `users` (`name`, `date`, `time`, `location`, `ticket_price`) VALUES(%s, %s, %s, %s, %s)"
+    value = (name, date, time, location, ticket_price)
+    cursor.execute(sql, value)
+    conn.commit()
+    return render_template('admin/createevent.html', success = 'Event created successfully')
  
+
+ #Users routes
+
+@app.route('/userDashboard')
+def userDashboard():
+    return (render_template('user/userDashboard.html'))
+
+@app.route('/allevents')
+def allevents():
+    # Create a cursor object to execute MySQL queries
+    cursor = conn.cursor()
+    # Execute the query to select all users from the users table
+    cursor.execute("SELECT * FROM users")
+    # Fetch all rows of the query result
+    rows = cursor.fetchall()
+    # Close the cursor
+    cursor.close()
+    return (render_template('user/allevents.html', rows=rows ))
+
+@app.route('/userbooking')
+def userbooking():
+    return (render_template('user/booking.html'))
+
+@app.route('/userprofile')
+def userProfile():
+    return (render_template('user/profile.html'))
+
+@app.route('//userlogout')
+def userLogout():
+    return (render_template('user/userlogout.html'))
+
 if __name__ == "__main__":
     app.run(debug=True)
