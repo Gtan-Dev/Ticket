8c082dd7a78296bcdb4829a9080667652c6d0bda
Gtan <105869793+Gtan-1@users.noreply.github.com>
2023-04-19 11:16:15 -0700

    displaying different records to user and admin dashboard

--- main.py
+++ main.py
@@ -26,23 +26,24 @@ def admin():
 def home():
     return render_template('home.html')
 
+#Regist new customer route
+
 @app.route('/regist', methods=['GET', 'POST'])
 def regist():
     username = request.form.get('username')
+    names = request.form.get('name')
     email = request.form.get('email')
     phone = request.form.get('phone')
+    address = request.form.get('address')
     password = request.form.get('password')
     confirm_password = request.form.get('confirm-password')
-
-    # Create a cursor object
-
     cursor = conn.cursor()
 
-    sql = "INSERT INTO `customer` (`username`, `email`, `phone`, `password`, `confirm_password`) VALUES(%s, %s, %s, %s, %s)"
-    value = (username, email, phone, password, confirm_password)
+    sql = "INSERT INTO `customer` (`username`, `names`, `email`, `phone`, `address`, `password`, `confirm_password`) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+    value = (username, names, email, phone, address, password, confirm_password)
     cursor.execute(sql, value)
     conn.commit()
-    return render_template('home.html', success = 'User created successfully')
+    return render_template('home.html', success = 'User has created successful.')
 
 @app.route('/events')
 def events():
@@ -62,28 +63,29 @@ def payment():
 
 @app.route('/setting')
 def setting():
-    return (render_template('admin-setting.html'))
-
-@app.route('/profile')
-def profile():
-    return (render_template('admin/profile.html'))
+    return (render_template('admin/admin-setting.html'))
 
 @app.route('/booking')
 def booking():
     return (render_template('admin/admin-booking.html'))
 
-@app.route('/customers')
-def customers():
-    return (render_template('admin/admin-customers.html'))
-
 #Route to retrieve events from database to admin page
 @app.route('/ad-events')
 def listEvents():
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users")
+    cursor.execute("SELECT * FROM events")
     rows = cursor.fetchall()
     cursor.close()
     return (render_template('admin/ad-events.html', rows=rows))
+
+#Route to retrieve customer from database to admin page
+@app.route('/customers')
+def retrieveCustomer():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM customer")
+    rows = cursor.fetchall()
+    cursor.close()
+    return (render_template('admin/admin-customers.html', rows=rows))
 
 
 @app.route('/createevent')
@@ -114,11 +116,11 @@ def store():
      # Create a cursor object
     cursor = conn.cursor()
 
-    sql = "INSERT INTO `users` (`name`, `date`, `time`, `location`, `ticket_price`) VALUES(%s, %s, %s, %s, %s)"
+    sql = "INSERT INTO `events` (`name`, `date`, `time`, `location`, `ticket_price`) VALUES(%s, %s, %s, %s, %s)"
     value = (name, date, time, location, ticket_price)
     cursor.execute(sql, value)
     conn.commit()
-    return render_template('admin/createevent.html', success = 'Event created successfully')
+    return render_template('admin/createevent.html', success = 'Event created successfully !')
  
 
  #Users routes
@@ -132,7 +134,7 @@ def allevents():
     # Create a cursor object to execute MySQL queries
     cursor = conn.cursor()
     # Execute the query to select all users from the users table
-    cursor.execute("SELECT * FROM users")
+    cursor.execute("SELECT * FROM events")
     # Fetch all rows of the query result
     rows = cursor.fetchall()
     # Close the cursor
@@ -143,9 +145,23 @@ def allevents():
 def userbooking():
     return (render_template('user/booking.html'))
 
+#retrieve users to user dashboard
 @app.route('/userprofile')
 def userProfile():
-    return (render_template('user/profile.html'))
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM customer")
+    rows = cursor.fetchall()
+    cursor.close()
+    return (render_template('user/profile.html', rows=rows))
+
+#view single record
+@app.route('/viewEvent/<event_id>')
+def viewEvent(event_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM events WHERE event_id = %s", (event_id,))
+    data = cursor.fetchall()[0]
+    cursor.close()
+    return (render_template('user/viewEvent.html', data=data))
 
 @app.route('//userlogout')
 def userLogout():
